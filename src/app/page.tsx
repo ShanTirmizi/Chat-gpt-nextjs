ac8b6d97d89669d3848a9165b24d8a683500b69f
Shan Tirmizi <tirmizishahnawaz@gmail.com>
2023-08-05 12:16:10 +0100

    add react query and api end point

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import Image from 'next/image'
+import ChatInput from '@/components/ChatInput';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold text-center">
-        Welcome to your AI assitant
+        Welcome to your AI assistant
       </h1>
+      <ChatInput />
     </main>
   )
 }
